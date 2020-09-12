86ef8c4893dcc83e7eab9632f20f7f55b00538a3
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-09-11 22:17:37 -0700

    ex3 completed, tests pass

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,9 +1,21 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    in_all = {}
+    for n in arrays[0]:
+        in_all[n] = 1
 
+    for array in arrays[1:]:
+        for n in array:
+            try:
+                if in_all[n]:
+                    in_all[n] += 1
+            except:
+                pass
+
+    result = []
+    for (k, v) in in_all.items():
+        if v == len(arrays):
+            result.append(k)
+        
     return result
 
 
@@ -15,3 +27,5 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+    
